9a763e3e3818e740523ec2bc2eee5e62ecd6d4a2
Alec Harley <alec.91@gmail.com>
2016-02-03 00:45:12 -0800

    Initial setup
    Runs all batch files, takes a normal 410C and
    enables wifi adb
    installs new boot animation
    deletes preinstalled apps in prep for google apps
    installs google apps
    installs and enables superuser

---
+++ initial.bat
@@ -0,0 +1,6 @@
+start /wait adbwifisetup.bat
+start /wait restore_wifi.bat
+start /wait install_bootanimation.bat
+start /wait delete_preinstalled.bat
+start /wait install_gapps.bat
+start /wait install_superuser.bat
